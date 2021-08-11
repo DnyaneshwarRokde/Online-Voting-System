c6a5338f93c6047c33ed473837f9778e03b10cc4
DnyaneshwarRokde <80511755+DnyaneshwarRokde@users.noreply.github.com>
2021-08-11 13:02:33 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+<title>main page</title>
+</head>
+<style>
+
+body{
+		margin:0;
+		padding:0;
+		background-image:linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url(2.jpg);
+		background-size:cover;
+		background-position:center;
+
+	}
+	table{
+	position:absolute;
+	
+	left:28%;
+	width:45%;
+	height:90%;
+	top:8%;
+	border-radius:20px;
+	border: solid orange;
+}
+table img{
+
+	border-radius:30px;
+	border:solid;
+}			
+table th{
+	font-size:25px;
+	border-bottom:solid orange;
+	color:white;
+	
+}
+table td{
+		font-size:25px;
+		color:white;
+}
+#btn{
+	width:80px;
+	border-radius:20px;
+	height:40px;
+	background-color:blue;
+}	
+</style>
+<table border=0	>
+	<tr >
+	<th colspan=3>Political Parties</th>
+	</tr>
+	<tr>
+		<td align="center"><img src="bjp.jpeg" width="120" height="80"></td>
+		<td >BJP</td>
+		<td ><input type="submit" id="btn" value="" ></td>
+	</tr>
+	<tr>
+		<td align="center"><img src="congress.jpeg" width="120" height="80"></td>
+		<td >Congress</td>
+		<td ><input type="submit" id="btn" value=""></td>
+	</tr>
+	<tr>
+		<td align="center"><img src="shivsena.jpeg" width="120" height="80"></td>
+		<td >Shivsena</td>
+		<td ><input type="submit" id="btn" value="" ></td>	
+	</tr>
+	<tr>
+		<td align="center"><img src="manase.jpg" width="120" height="80"></td>
+		<td >Manase</td>
+		<td ><input type="submit" id="btn" value="" ></td>
+	</tr>
+	<tr>
+		<td align="center"><img src="ncp.png" width="120" height="80"></td>
+		<td >NCP</td>
+		<td ><input type="submit" id="btn" value="" ></td>
+	</tr>
+	
+</table>
+</body>
+</body>
+</html>
